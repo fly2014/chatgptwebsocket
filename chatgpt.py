f8d3998cde48d40c380ff381db5e1239e7bbe1d5
flying <flying@glcool>
2023-03-02 13:59:24 +0800

    a

--- chatgpt.py
+++ chatgpt.py
@@ -15,8 +15,6 @@ import threading
 from time import sleep
 class MainHandler(tornado.web.RequestHandler):
    def get(self):
-         self.set_secure_cookie("name", 'laia',expires_days=None)
-         self.set_secure_cookie("token",'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',expires_days=None)
          name = self.get_secure_cookie('name')
          token = self.get_secure_cookie('token')
          status = 'hello 请输入token 连接！'
